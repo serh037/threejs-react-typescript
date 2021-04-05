dd1385d1f1e0dd99308c6eadecb0e5bc4cacd4d7
Serj Alekian <sergey.alekyan@nrg-soft.com>
2021-04-06 01:49:02 +0300

    Added ThreeJS

--- src/App.component.tsx
+++ src/App.component.tsx
@@ -1,8 +1,41 @@
-function App() {
+import * as THREE from "three";
+import { useRef, useState } from "react";
+import { Canvas, useFrame } from "@react-three/fiber";
+import { useWindowWidth, useWindowHeight } from "react-window-size-hooks";
+
+function Box(props: JSX.IntrinsicElements["mesh"]) {
+  const mesh = useRef<THREE.Mesh>(null!);
+  const [hovered, setHover] = useState(false);
+  const [active, setActive] = useState(false);
+
+  useFrame(() => (mesh.current.rotation.x += 0.01));
+
   return (
-    <div className="App">
-      three js
-    </div>
+    <mesh
+      {...props}
+      ref={mesh}
+      scale={active ? 1.5 : 1}
+      onClick={() => setActive(!active)}
+      onPointerOver={() => setHover(true)}
+      onPointerOut={() => setHover(false)}
+    >
+      <boxGeometry args={[1, 1, 1]} />
+      <meshStandardMaterial color={hovered ? "hotpink" : "orange"} />
+    </mesh>
+  );
+}
+
+function App() {
+  const width = useWindowWidth();
+  const height = useWindowHeight();
+
+  return (
+    <Canvas style={{ height, width }}>
+      <ambientLight />
+      <pointLight position={[10, 10, 10]} />
+      <Box position={[-1.2, 0, 0]} />
+      <Box position={[1.2, 0, 0]} />
+    </Canvas>
   );
 }
 
